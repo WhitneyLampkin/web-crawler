dd76ab64f2a7c8c442ff34719df2349a2d1497cd
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-12-14 21:14:50 -0600

    full app with example export.csv file

--- cmd/stockx-shoe-scraper/main.go
+++ cmd/stockx-shoe-scraper/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/gocolly/colly"
 )
@@ -12,12 +15,35 @@ type Shoe struct {
 }
 
 func main() {
+	file, err := os.Create("export.csv")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	headers := []string{"Name", "Price"}
+	writer.Write(headers)
+
 	c := colly.NewCollector(
 		colly.AllowedDomains("stockx.com"),
 	)
 
-	c.OnHTML(".css-3lpefb", func(e *colly.HTMLElement) {
-		fmt.Println(e.Text)
+	c.OnHTML(".css-1ibvugw-GridProductTileContainer", func(e *colly.HTMLElement) {
+		shoe := Shoe{}
+		shoe.Name = e.ChildText(".css-3lpefb")
+		shoe.Price = e.ChildText(".css-9ryi0c")
+		row := []string{shoe.Name, shoe.Price}
+		writer.Write(row)
+		fmt.Println(shoe.Name, "|", shoe.Price)
+	})
+
+	c.OnHTML(".css-12da55z-PaginationButton", func(e *colly.HTMLElement) {
+		nextPage := e.Request.AbsoluteURL(e.Attr("href"))
+		c.Visit(nextPage)
 	})
 
 	c.OnRequest(func(r *colly.Request) {
