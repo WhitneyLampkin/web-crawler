867fcbddb8a4ff36832595146241f1833d9242c5
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-12-14 20:39:30 -0600

    adding code to scrape StockX site for sneakers

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	c := colly.NewCollector(
+		colly.AllowedDomains("stockx.com"),
+	)
+
+	c.OnHTML(".css-3lpefb", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println(r.StatusCode)
+	})
+
+	c.Visit("https://stockx.com/sneakers")
+
+	fmt.Println("Done")
 }
